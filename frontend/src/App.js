9d4a4eed2a0f7e0dd1e3250596bf6e0af751de8b
Chanda5 <chandapiwamalema5@gmail.com>
2024-05-14 17:27:22 +0200

    tweaks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Componets/Navbar/Navbar';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCategory  from './Pages/ShopCategory';
+
 
 function App() {
   return (
     <div >
-      <Navbar></Navbar>
+      <BrowserRouter>
+     
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCategory category="mens"/>}/>
+        <Route path='/womens' element={<ShopCategory category="womens"/>}/>
+        <Route path='/kids' element={<ShopCategory category="kids"/>}/>
+        
+        
+        
+
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
